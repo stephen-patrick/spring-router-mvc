1540caf4d75c68e3ef447aae5c38a2557872f9ed
stephen <stephen.enright@gmail.com>
2017-02-26 03:06:17 +0000

    Added new method to route tag to take route parameters as csv string

--- src/main/java/spring/router/mvc/tags/thymeleaf/RouterExpression.java
+++ src/main/java/spring/router/mvc/tags/thymeleaf/RouterExpression.java
@@ -1,5 +1,6 @@
 package spring.router.mvc.tags.thymeleaf;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import spring.router.mvc.RouteHelper;
@@ -24,5 +25,26 @@ public class RouterExpression {
 	public String reverseByName(String name, Map<String, Object> params) {
 		return RouteHelper.reverse(name, new RouteParameterCollection(params));
 	}
+	
+	
+	public String reverseByName(String name, String paramCsv) {
+		return RouteHelper.reverse(name, new RouteParameterCollection(
+				fromCsvValue(paramCsv)));
+	}
+	
+	private static Map<String, Object> fromCsvValue(String value) {
+		Map<String, Object> valueMap = new HashMap<String, Object>();
+		String[] parts = value.split(",");
+
+		for (String part : parts) {
+			String[] valueParts = part.split("=");
+			valueMap.put(valueParts[0], valueParts[1]);
+
+		}
+
+		return valueMap;
+
+	}
+
 
 }
